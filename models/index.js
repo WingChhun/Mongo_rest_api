4ceed482d48706c87dae711d0fdd701b680a3ebe
James Chhun <capt.wing.chhun@gmail.com>
2018-02-04 09:39:49 -0800

    added mongodb and connection

---
+++ models/index.js
@@ -0,0 +1,21 @@
+var mongoose = require("mongoose"),
+Schema = mongoose.Schema,
+DB_URL = process.env.DB_URL || "mongodb://localhost/sports_api";
+
+
+
+mongoose.set('debug',true);
+mongoose.connect(DB_URL);
+mongoose.connection
+	.once("open",()=> {
+		//Display to user successfully connected to DB
+		console.log("connection made to db")
+	})
+	.on("error", (error)=> {
+		//Display Connection to DB error
+		console.log("ERROR connecting to db")
+	})
+mongoose.Promise = Promise; //allow use of promises
+
+
+var sportSchema = new Schema;
